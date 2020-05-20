10e6ef1238ca8359b49f74091a81e9faeeb8aca8
nbogie <neillbogie@googlemail.com>
2020-05-20 17:47:24 +0100

    Create scores.js

---
+++ challenge-high-score-tables/scores.js
@@ -0,0 +1,31 @@
+ let allCountryScores = [
+    {
+      name: "Ethiopia", 
+      scores:  [ {n: "Hanif", s: 999999999}, {n: "lucy", s: "4134234"}, {n: "DWH", s: 9999},  {n: "Sub", s: 9990} ]
+    },     
+    {
+      name: "Scotland", 
+      scores: [   , {n: "Neill", s: 999999},{n: "groundkeeper willie", s: 4000}, {n: "braveheart", s: -200}]
+    },{
+      name: "Brazil", 
+      scores: [ {n: "Mozart", s: 999}]
+    },
+    {
+      name: "Colombia", 
+      scores: [   {n: "Melanie", s: "99999999"}, {n: "Maria", s: 6000}, {n: "Ali", s: 5000}]
+    }, 
+    {
+      name: "Turkey", 
+      scores: [   {n: "mahmut", s: 1000  }, {n: "morat", s: 999  }, {n: "selim", s: 900  }, ]
+    },
+    {
+      name: "Iran", 
+      scores: [ {n: "arosha", s: 5550  },  {n: "zahra", s: 3000  }, {n: "nader", s: 2000  }, {n: "Bani", s: 1999  } ]
+    },
+    {
+      name: "Bangladesh", 
+      scores: [ {n: "rayhan", s: 18238123}, {n: "ali", s: 5400000}, {n: "rahman", s: 700200} , , {n: "Chris", s: 10} ]
+    },
+  ];
+  
+  export default allCountryScores;
