ae12f8166525fb41fd83f28b9170c96899e19bf1
nbogie <neillbogie@googlemail.com>
2020-05-21 18:10:57 +0100

    high score tables: minor tidy of scores.js
    had a rogue comma

--- challenge-high-score-tables/scores.js
+++ challenge-high-score-tables/scores.js
@@ -5,18 +5,18 @@
     },     
     {
       name: "Scotland", 
-      scores: [   , {n: "Neill", s: 999999},{n: "groundkeeper willie", s: 4000}, {n: "braveheart", s: -200}]
+      scores: [  {n: "Neill", s: 999999},{n: "groundkeeper willie", s: 4000}, {n: "braveheart", s: -200}]
     },{
       name: "Brazil", 
       scores: [ {n: "Mozart", s: 999}]
     },
     {
       name: "Colombia", 
-      scores: [   {n: "Melanie", s: "99999999"}, {n: "Maria", s: 6000}, {n: "Ali", s: 5000}]
+      scores: [ {n: "Melanie", s: "99999999"}, {n: "Maria", s: 6000}, {n: "Ali", s: 5000}]
     }, 
     {
       name: "Turkey", 
-      scores: [   {n: "mahmut", s: 1000  }, {n: "morat", s: 999  }, {n: "selim", s: 900  }, ]
+      scores: [ {n: "mahmut", s: 1000  }, {n: "morat", s: 999  }, {n: "selim", s: 900  }, ]
     },
     {
       name: "Iran", 
