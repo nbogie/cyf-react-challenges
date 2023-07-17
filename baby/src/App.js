873546bfa16257a09f7a37fc193a1b10d30eba91
Joseph Mwanza <jmwanzaj@yahoo.com>
2023-07-17 20:44:07 +0100

    added search function

--- baby/src/App.js
+++ baby/src/App.js
@@ -1,13 +1,21 @@
 
 import './App.css';
 import Sortednames from './sorted-names';
+import Search from './Search';
+import { useState } from 'react';
+
 function App() {
-  
+const [filterNames, setFilterNames] = useState("");
+function setFilter (e) {
+  setFilterNames(e.target.value.toLowerCase()) 
+}
   return (
     
       <div>
+
         <div className='appContainer'>
-        {<Sortednames />}
+          <Search setFilter = {setFilter}/>
+        <Sortednames filterNames = {filterNames}/>
         </div>
       </div>
       
--- baby/src/sorted-names.js
+++ baby/src/sorted-names.js
@@ -1,5 +1,5 @@
 import jsonData from './data/babyNamesData.json';
-function Sortednames() {
+function Sortednames({filterNames}) {
   return (
     <div>
     {jsonData.sort(function (a,b) {
@@ -7,6 +7,7 @@ function Sortednames() {
       return (alphabeticSort);
 
     })
+    .filter(el => el.name.toLowerCase().includes(filterNames))
     .map((element) => {if (element.sex === "m") {return (<var><button className='babyNamesBlue' >{element.name}</button></var>);}
     else {return (<var><button className='babyNamesPink' >{element.name}</button></var>)}})}
 </div>
